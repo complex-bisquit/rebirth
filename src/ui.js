01d9d721c4ba956d1fa9146ba4f46080d19ddc8f
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2026-07-29 12:10:35 +0200

    Actuslly push the moves

--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,14 @@
-export function draw(state, moves) {
+/** @import { ClientState } from "boardgame.io/src/client/client" */
+/** @import { Game } from "./Game" */
+
+export function draw(
+  /** @type {ClientState<[ReturnType<Game["setup"]>]>} */
+  state,
+  /** @type{Record<string, (...args: any[]) => void>} */
+  moves,
+) {
   const canvas = document.getElementById("canvas")
   const ctx = canvas.getContext("2d")
-  console.log(moves)
+
   // draw here
 }
