f3b5a0b283d053c9ff5225de23ae387fab3d4bd9
DESKTOP-IC80259\TN <jan+1@opencreek.tech>
2026-07-30 11:49:18 +0200

    wip

--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,8 @@
 /** @import { ClientState } from "boardgame.io/src/client/client" */
 /** @import { Game } from "./Game" */
 
+import { onClick } from "./canvas"
+
 export function draw(
   /** @type {ClientState<[ReturnType<Game["setup"]>]>} */
   state,
@@ -10,21 +12,24 @@ export function draw(
   const canvas = document.getElementById("canvas")
   const ctx = canvas.getContext("2d")
   console.dir(state)
-  hexagon(ctx, state.G, 530, 25)
-  const a = (25 / 2) * Math.sqrt(3)
-  const b = 25 / 2
-  ctx.fillStyle = "rgba(255,0,0,0.5)"
-  ctx.fillRect(530 - a, 25, 498, 9 * 2 * b + 10 * 4 * b) //9 für even, 10 für uneven
-}
-
-function mouseHandler(mouseX, mouseY, x, y, state) {
-  let curSmallestLine = Number.MAX_SAFE_INTEGER
+  let x = 530
+  let y = 25
   const a = (y / 2) * Math.sqrt(3)
   const b = y / 2
+  hexagon(ctx, state.G, x, y, a, b)
+  ctx.fillStyle = "rgba(255,0,0,0.5)"
+  ctx.fillRect(x - a, y, 498, 9 * 2 * b + 10 * 4 * b) //9 für even, 10 für uneven
+  onClick(x - a, y, 498, 9 * 2 * b + 10 * 4 * b, (mouseX, mouseY) => {
+    mouseHandler(mouseX, mouseY, x, y, a, b, state.G)
+  })
+}
+
+function mouseHandler(mouseX, mouseY, x, y, a, b, state) {
+  let curSmallestLine = Number.MAX_SAFE_INTEGER
   const middle = []
   for (let countRow = 1; countRow < 20; countRow++) {
     for (let i = 1; i < 12; i++) {
-      middle.push({ x: x - 1, y: y + 2 * b - 1 })
+      middle.push({ x: x - 1, y: y + 2 * b - 1, id: state:G.tileID})
       if ((countRow & 1) === 0 && countRow !== 1) {
         y += 3 * b
         x -= 23 * a
@@ -43,9 +48,7 @@ function mouseHandler(mouseX, mouseY, x, y, state) {
     }
   }
 }
-function hexagon(ctx, board, x, y) {
-  const a = (y / 2) * Math.sqrt(3)
-  const b = y / 2
+function hexagon(ctx, board, x, y, a, b) {
   for (let countRow = 1; countRow < 20; countRow++) {
     for (let i = 1; i < 12; i++) {
       ctx.beginPath()
