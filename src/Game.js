3bf44b30579af93414d2fdc0b044cc6cb2d6919b
DESKTOP-IC80259\TN <jan+1@opencreek.tech>
2026-07-31 17:21:02 +0200

    wip

--- src/Game.js
+++ src/Game.js
@@ -42,27 +42,32 @@ function getNeighbours(board) {
 /** @type {Game} */
 export const Game = {
   setup: ({ random, ctx }) => {
+    const player1 = {
+      score: 0
+      bag: []
+    }
     let score1 = 0,
       score2 = 0,
       score3 = 0,
       score4 = 0
-    let bag1,
+    let bag1
       bag2,
       bag3,
       bag4 = []
-    bag4.length = 36
-    bag4
+    player1.bag.length = 36
+    player1.bag
       .fill("F", 0, 12)
       .fill("E", 12, 24)
       .fill("D1", 24, 27)
       .fill("D2", 27, 30)
       .fill("D3", 30, 33)
       .fill("D4", 33, 36)
-    bag1 = random.Shuffle(bag4)
-    bag2 = random.Shuffle(bag4)
+    player1.bag = random.Shuffle(player1.bag)
+    bag2 = random.Shuffle(player1.bag
+    )
     bag3 = random.Shuffle(bag4)
     bag4 = random.Shuffle(bag4)
-    bag1.pop()
+    bag1.pop()//delete 2 elements
     bag1.pop()
     bag2.pop()
     bag2.pop()
@@ -74,7 +79,6 @@ export const Game = {
     let handTile2 = bag2.pop()
     let handTile3 = bag3.pop()
     let handTile4 = bag4.pop()
-    console.log(handTile1)
     console.log(bag1, bag2, bag3, bag4)
     for (let i = 0; i < 36; i++) {}
     // W = Wasser
