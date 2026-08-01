0297e382851b49002909f89bfc0d71820706be8b
DESKTOP-IC80259\TN <jan+1@opencreek.tech>
2026-08-01 11:13:08 +0200

    wip

--- src/ui.js
+++ src/ui.js
@@ -65,9 +65,8 @@ function mouseHandler(ctx, mouseX, mouseY, x, y, a, b, state, moves) {
     ctx.fillStyle = "white"
     ctx.fillRect(middle[neighbour - 1].x, middle[neighbour - 1].y, 5, 5)
   }
-	if () {
-
-	}
+  if (state.G[smallID]) {
+  }
 }
 
 function hexagon(ctx, board, x, y, a, b) {
