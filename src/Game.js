9c70b24c29a98b46a51181a3e9c2937c92e7a419
DESKTOP-IC80259\TN <jan+1@opencreek.tech>
2026-08-03 10:28:08 +0200

    wip

--- src/Game.js
+++ src/Game.js
@@ -165,20 +165,23 @@ WWWFCDPWWWW
         state.G.board[id].occ != null ||
         state.G.board[id].type === "W" ||
         state.G.board[id].type === "C" ||
-        state.G.board[id].type === "M" ||
+        state.G.board[id].type === "M"
       ) {
         return INVALID_MOVE
-      } else if ((state.G.board[id].type === "D") && (state.G.player[state.ctx.currentPlayer].handTile === D1 ||
-        state.G.player[state.ctx.currentPlayer].handTile === D2 ||
-        state.G.player[state.ctx.currentPlayer].handTile === D3 ||
-        state.G.player[state.ctx.currentPlayer].handTile === D4 ||
-       ) {
-        continue
-      } else if (state.G.player[state.ctx.currentPlayer].handTile !==
-          state.G.board[id].type) {
-            return INVALID_MOVE
-          }
-
+      } else if (
+        state.G.board[id].type === "D" &&
+        (state.G.player[state.ctx.currentPlayer].handTile === "D1" ||
+          state.G.player[state.ctx.currentPlayer].handTile === "D2" ||
+          state.G.player[state.ctx.currentPlayer].handTile === "D3" ||
+          state.G.player[state.ctx.currentPlayer].handTile === "D4")
+      ) {
+        pass
+      } else if (
+        state.G.player[state.ctx.currentPlayer].handTile !==
+        state.G.board[id].type
+      ) {
+        return INVALID_MOVE
+      }
 
       state.G.board[id].occ = state.ctx.currentPlayer
 
