8a5826f7b4ff99a8a8976c2431fb08bfbf0e8c41
DESKTOP-IC80259\TN <jan+1@opencreek.tech>
2026-08-05 09:48:19 +0200

    wip

--- src/Game.js
+++ src/Game.js
@@ -269,5 +269,14 @@ WWWFCDPWWWW
 
   disableUndo: true,
 
-  endIf: ({ G, ctx, random }) => {},
+  endIf: ({ G, ctx, random }) => {
+    const player = state.G.player
+    if (
+      player[0].bag.length === 0 &&
+      player[1].bag.length === 0 &&
+      player[2].bag.length === 0 &&
+      player[3].bag.length === 0
+    ) {
+    }
+  },
 }
