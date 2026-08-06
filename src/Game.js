5f1d554e9af320c46a58934d7d7a9b5566a22c1f
DESKTOP-IC80259\TN <jan+1@opencreek.tech>
2026-08-06 12:03:27 +0200

    wip

--- src/Game.js
+++ src/Game.js
@@ -339,18 +339,19 @@ WWWFCDPWWWW
 
   disableUndo: true,
 
-  endIf: ({ G, ctx, random }) => {
-    const player = G.player
-    for (let i = 0; i < ctx.numPlayers; i++) {
+  endIf: ({ state, random }) => {
+    const player = state.G.player
+    for (let i = 0; i < state.ctx.numPlayers; i++) {
       if (player[i].handTile !== undefined) {
         return null
       }
     }
+    castleScoring(state)
     let bestPlayer = {
       bigScore: 0,
       id: 0,
     }
-    for (let i = 0; i < ctx.numPlayers; i++) {
+    for (let i = 0; i < state.ctx.numPlayers; i++) {
       if (player[i].score > bestPlayer.bigScore) {
         bestPlayer.bigScore = player[i].score
         bestPlayer.id = player[i].id
