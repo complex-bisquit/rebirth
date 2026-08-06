45b818f687a926cf22185e3a88b0bc14a47fd833
DESKTOP-IC80259\TN <jan+1@opencreek.tech>
2026-08-06 11:28:05 +0200

    wip

--- src/Game.js
+++ src/Game.js
@@ -60,7 +60,6 @@ function searchScore(state, curID) {
     let wasEmpty = 0
     for (let goodTile of visited) {
       if (goodTile.occ === null) {
-        //MAKE THIS THING WORK
         wasEmpty += 1
         break
       }
@@ -275,9 +274,30 @@ WWWFCDPWWWW
 
       state.G.board[id].occ = state.ctx.currentPlayer
 
+      let occNearCastle = Array(state.ctx.numPlayers).fill(0)
+
+      for (let checkCastle of state.G.board[id].neighbours) {
+        if (checkCastle.type === "C" && checkCastle.occ === null) {
+          checkCastle.occ = state.ctx.currentPlayer
+          checkCastle.firstPlayerID = state.ctx.currentPlayer
+        }
+        if (checkCastle.type === "C") {
+          for (let neighboursC of state.G.board[checkCastle.id - 1]
+            .neighbours) {
+            if (neighboursC.occ !== null) {
+              occNearCastle[neighboursC.occ]++
+            }
+            let wasEmpty = 0
+            if (goodTile.occ === null) {
+              wasEmpty += 1
+              break
+            }
+          }
+        }
+      }
+
       state.G.player[state.ctx.currentPlayer].handTile =
         state.G.player[state.ctx.currentPlayer].bag.shift()
-      console.log("MOIN")
       searchScore(state, id)
     },
   },
